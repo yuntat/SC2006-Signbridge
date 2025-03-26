5b6d2a0b19b49e9b2a33c056be91ace8994274ad
Wei Ming <alvinwm0523@gmail.com>
2025-03-26 17:11:50 +0800

    update

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -4,92 +4,97 @@ import {
   Image,
   StyleSheet,
   StatusBar,
-  Dimensions
+  Dimensions,
+  View,
+  ScrollView
 } from "react-native";
 import { Block, Button, Text, theme } from "galio-framework";
-
-const { height, width } = Dimensions.get("screen");
+import { useWindowDimensions } from "react-native";
 
 import argonTheme from "../constants/Theme";
 import Images from "../constants/Images";
 
-class Onboarding extends React.Component {
-  render() {
-    const { navigation } = this.props;
+const Onboarding = ({ navigation }) => {
+  const { height, width } = useWindowDimensions();
 
-    return (
-      <Block flex style={styles.container}>
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <Block flex style={[styles.container, { minHeight: height }]}>
         <StatusBar hidden />
         <Block flex center>
-        <ImageBackground
+          <ImageBackground
             source={Images.Onboarding}
-            style={{ height, width, zIndex: 1 }}
+            style={[styles.backgroundImage, { height: height, width }]}
           />
         </Block>
-        <Block center>
+        <Block center style={styles.overlayContent}>
           <Image source={Images.LogoOnboarding} style={styles.logo} />
-        </Block>
-        <Block flex space="between" style={styles.padded}>
-            <Block flex space="around" style={{ zIndex: 2 }}>
-              <Block style={styles.title}>
-                <Block>
-                  <Text color="white" size={60}>
-                    Design
-                  </Text>
-                </Block>
-                <Block>
-                  <Text color="white" size={60}>
-                    System
-                  </Text>
-                </Block>
-                <Block style={styles.subTitle}>
-                  <Text color="white" size={16}>
-                    Fully coded React Native components.
-                  </Text>
-                </Block>
+          <Block flex space="around" style={{ zIndex: 2 }}>
+            <Block style={styles.title}>
+              <Block>
+                <Text color="white" size={width > 600 ? 80 : 60}>
+                  Design
+                </Text>
               </Block>
-              <Block center>
-                <Button
-                  style={styles.button}
-                  color={argonTheme.COLORS.SECONDARY}
-                  onPress={() => navigation.navigate("App")}
-                  textStyle={{ color: argonTheme.COLORS.BLACK }}
-                >
-                  Get Started
-                </Button>
+              <Block>
+                <Text color="white" size={width > 600 ? 80 : 60}>
+                  System
+                </Text>
               </Block>
+              <Block style={styles.subTitle}>
+                <Text color="white" size={16}>
+                  Fully coded React Native components.
+                </Text>
+              </Block>
+            </Block>
           </Block>
         </Block>
+        <Block center style={styles.buttonContainer}>
+          <Button
+            style={[styles.button, { width: width * 0.8 }]}
+            color={argonTheme.COLORS.SECONDARY}
+            onPress={() => navigation.replace("App")}
+            textStyle={{ color: argonTheme.COLORS.BLACK }}
+          >
+            Get Started
+          </Button>
+        </Block>
       </Block>
-    );
-  }
-}
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.COLORS.BLACK
+    backgroundColor: theme.COLORS.BLACK,
   },
-  padded: {
-    paddingHorizontal: theme.SIZES.BASE * 2,
-    position: "relative",
-    bottom: theme.SIZES.BASE,
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    zIndex: 1,
+  },
+  overlayContent: {
+    position: "absolute",
+    top: "30%",
     zIndex: 2,
   },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 30,
+    zIndex: 3,
+  },
   button: {
-    width: width - theme.SIZES.BASE * 4,
     height: theme.SIZES.BASE * 3,
     shadowRadius: 0,
-    shadowOpacity: 0
+    shadowOpacity: 0,
   },
   logo: {
     width: 200,
     height: 60,
-    zIndex: 2,
-    position: 'relative',
-    marginTop: '-50%'
+    marginBottom: 20,
   },
   title: {
-    marginTop:'-5%'
+    alignItems: 'flex-start',
   },
   subTitle: {
     marginTop: 20
