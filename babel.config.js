8d70bb73ea856999ebcd2f51f94a22c9ee3df89a
YunTat <yheng015@e.ntu.edu.sg>
2025-03-29 18:15:23 +0800

    m

--- babel.config.js
+++ babel.config.js
@@ -3,23 +3,16 @@ module.exports = function (api) {
 
   return {
     presets: [
-      "babel-preset-expo",  // Expo preset for React Native compatibility
-      "@babel/preset-react",  // To handle React JSX syntax
-      [
-        "@babel/preset-env",  // Support for modern JavaScript syntax
-        {
-          targets: "> 0.25%, not dead",  // Supports modern browsers
-          useBuiltIns: "entry",  // Polyfill for modern JS features
-          corejs: 3,  // Use CoreJS version 3 for polyfills
-        },
-      ],
+      'babel-preset-expo',  // For Expo compatibility
+      '@babel/preset-env',  // Support for modern JavaScript syntax
+      '@babel/preset-react', // To handle React JSX syntax
     ],
     plugins: [
-      "react-native-reanimated/plugin",  // If you're using React Native Reanimated
+      'react-native-reanimated/plugin',  // For React Native Reanimated
       [
-        "module-resolver",
+        'module-resolver',
         {
-          extensions: [".tsx", ".ts", ".js", ".json"],  // Support for TypeScript, JS, and JSON
+          extensions: ['.tsx', '.ts', '.js', '.json'],  // Support for TypeScript, JS, and JSON
         },
       ],
     ],
