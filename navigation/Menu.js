913c7e72c338a65a8d928330fb20f92555d862b6
Sandwhich253989 <avanesh165@gmail.com>
2025-04-08 13:12:12 +0800

    Added Text to sign translator feature/compoenent

--- navigation/Menu.js
+++ navigation/Menu.js
@@ -13,7 +13,7 @@ function CustomDrawerContent({
   state,
   ...rest
 }) {
-  const screens = ["Home", "Profile", "Account", "Elements", "Articles"];
+  const screens = ["Home", "Profile", "Account", "Elements", "Articles","Text to Sign"];
   return (
     <Block
       style={styles.container}
--- navigation/Screens.js
+++ navigation/Screens.js
@@ -1,10 +1,8 @@
-import { Animated, Dimensions, Easing } from "react-native";
+import {Dimensions} from "react-native";
 // header for screens
-import { Header, Icon } from "../components";
-import { argonTheme, tabs } from "../constants";
+import {Header} from "../components";
 
 import Articles from "../screens/Articles";
-import { Block } from "galio-framework";
 // drawer
 import CustomDrawerContent from "./Menu";
 import Elements from "../screens/Elements";
@@ -16,286 +14,310 @@ import Pro from "../screens/Pro";
 import Profile from "../screens/Profile";
 import React from "react";
 import Register from "../screens/Register";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createStackNavigator } from "@react-navigation/stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import {createStackNavigator} from "@react-navigation/stack";
+import SignTranslator from "../screens/SignTranslator";
 
 console.log("Rendering Screens.js");
 
-const { width } = Dimensions.get("screen");
+const {width} = Dimensions.get("screen");
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 
 function ElementsStack(props) {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        mode: "card",
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen
-        name="Elements"
-        component={Elements}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header title="Elements" navigation={navigation} scene={scene} />
-          ),
-          cardStyle: { backgroundColor: "#F8F9FE" },
-        }}
-      />
-      <Stack.Screen
-        name="Pro"
-        component={Pro}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title=""
-              back
-              white
-              transparent
-              navigation={navigation}
-              scene={scene}
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                mode: "card",
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen
+                name="Elements"
+                component={Elements}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header title="Elements" navigation={navigation} scene={scene}/>
+                    ),
+                    cardStyle: {backgroundColor: "#F8F9FE"},
+                }}
             />
-          ),
-          headerTransparent: true,
-        }}
-      />
-    </Stack.Navigator>
-  );
+            <Stack.Screen
+                name="Pro"
+                component={Pro}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header
+                            title=""
+                            back
+                            white
+                            transparent
+                            navigation={navigation}
+                            scene={scene}
+                        />
+                    ),
+                    headerTransparent: true,
+                }}
+            />
+        </Stack.Navigator>
+    );
 }
 
 function ArticlesStack(props) {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        mode: "card",
-        headerShown: "screen",
-      }}
-    >
-      <Stack.Screen
-        name="Articles"
-        component={Articles}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header title="Articles" navigation={navigation} scene={scene} />
-          ),
-          cardStyle: { backgroundColor: "#F8F9FE" },
-        }}
-      />
-      <Stack.Screen
-        name="Pro"
-        component={Pro}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title=""
-              back
-              white
-              transparent
-              navigation={navigation}
-              scene={scene}
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                mode: "card",
+                headerShown: "screen",
+            }}
+        >
+            <Stack.Screen
+                name="Articles"
+                component={Articles}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header title="Articles" navigation={navigation} scene={scene}/>
+                    ),
+                    cardStyle: {backgroundColor: "#F8F9FE"},
+                }}
             />
-          ),
-          headerTransparent: true,
-        }}
-      />
-    </Stack.Navigator>
-  );
+            <Stack.Screen
+                name="Pro"
+                component={Pro}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header
+                            title=""
+                            back
+                            white
+                            transparent
+                            navigation={navigation}
+                            scene={scene}
+                        />
+                    ),
+                    headerTransparent: true,
+                }}
+            />
+        </Stack.Navigator>
+    );
 }
 
 function ProfileStack(props) {
-  return (
-    <Stack.Navigator
-      initialRouteName="Profile"
-      screenOptions={{
-        mode: "card",
-        headerShown: "screen",
-      }}
-    >
-      <Stack.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              transparent
-              white
-              title="Profile"
-              navigation={navigation}
-              scene={scene}
+    return (
+        <Stack.Navigator
+            initialRouteName="Profile"
+            screenOptions={{
+                mode: "card",
+                headerShown: "screen",
+            }}
+        >
+            <Stack.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header
+                            transparent
+                            white
+                            title="Profile"
+                            navigation={navigation}
+                            scene={scene}
+                        />
+                    ),
+                    cardStyle: {backgroundColor: "#FFFFFF"},
+                    headerTransparent: true,
+                }}
             />
-          ),
-          cardStyle: { backgroundColor: "#FFFFFF" },
-          headerTransparent: true,
-        }}
-      />
-      <Stack.Screen
-        name="Pro"
-        component={Pro}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title=""
-              back
-              white
-              transparent
-              navigation={navigation}
-              scene={scene}
+            <Stack.Screen
+                name="Pro"
+                component={Pro}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header
+                            title=""
+                            back
+                            white
+                            transparent
+                            navigation={navigation}
+                            scene={scene}
+                        />
+                    ),
+                    headerTransparent: true,
+                }}
             />
-          ),
-          headerTransparent: true,
-        }}
-      />
-    </Stack.Navigator>
-  );
+        </Stack.Navigator>
+    );
 }
 
 function HomeStack(props) {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        mode: "card",
-        headerShown: "screen",
-      }}
-    >
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title="Home"
-              search
-              options
-              navigation={navigation}
-              scene={scene}
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                mode: "card",
+                headerShown: "screen",
+            }}
+        >
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header
+                            title="Home"
+                            search
+                            options
+                            navigation={navigation}
+                            scene={scene}
+                        />
+                    ),
+                    cardStyle: {backgroundColor: "#F8F9FE"},
+                }}
             />
-          ),
-          cardStyle: { backgroundColor: "#F8F9FE" },
-        }}
-      />
-      <Stack.Screen
-        name="Pro"
-        component={Pro}
-        options={{
-          header: ({ navigation, scene }) => (
-            <Header
-              title=""
-              back
-              white
-              transparent
-              navigation={navigation}
-              scene={scene}
+            <Stack.Screen
+                name="Pro"
+                component={Pro}
+                options={{
+                    header: ({navigation, scene}) => (
+                        <Header
+                            title=""
+                            back
+                            white
+                            transparent
+                            navigation={navigation}
+                            scene={scene}
+                        />
+                    ),
+                    headerTransparent: true,
+                }}
             />
-          ),
-          headerTransparent: true,
-        }}
-      />
-    </Stack.Navigator>
-  );
+        </Stack.Navigator>
+    );
 }
 
 export default function OnboardingStack(props) {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        mode: "card",
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen
-        name="Onboarding"
-        component={Onboarding}
-        option={{
-          headerTransparent: true,
-        }}
-      />
-      {/* Add this new screen */}
-      <Stack.Screen 
-        name="Register" 
-        component={Register} 
-      />
-      <Stack.Screen 
-        name="App" 
-        component={AppStack} />
-      <Stack.Screen 
-        name="SignBridgeMain" 
-        component={SignBridgeMain} />
-    </Stack.Navigator>
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                mode: "card",
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen
+                name="Onboarding"
+                component={Onboarding}
+                option={{
+                    headerTransparent: true,
+                }}
+            />
+            {/* Add this new screen */}
+            <Stack.Screen
+                name="Register"
+                component={Register}
+            />
+            <Stack.Screen
+                name="App"
+                component={AppStack}/>
+            <Stack.Screen
+                name="SignBridgeMain"
+                component={SignBridgeMain}/>
+        </Stack.Navigator>
 
-  );
+    );
 }
 
 
 function AppStack(props) {
-  return (
-    <Drawer.Navigator
-      style={{ flex: 1 }}
-      drawerContent={(props) => <CustomDrawerContent {...props} />}
-      drawerStyle={{
-        backgroundColor: "white",
-        width: width * 0.8,
-      }}
-      drawerContentOptions={{
-        activeTintcolor: "white",
-        inactiveTintColor: "#000",
-        activeBackgroundColor: "transparent",
-        itemStyle: {
-          width: width * 0.75,
-          backgroundColor: "transparent",
-          paddingVertical: 16,
-          paddingHorizonal: 12,
-          justifyContent: "center",
-          alignContent: "center",
-          alignItems: "center",
-          overflow: "hidden",
-        },
-        labelStyle: {
-          fontSize: 18,
-          marginLeft: 12,
-          fontWeight: "normal",
-        },
-      }}
-      initialRouteName="Home"
-    >
-      <Drawer.Screen
-        name="Home"
-        component={HomeStack}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Drawer.Screen
-        name="Profile"
-        component={ProfileStack}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Drawer.Screen
-        name="Account"
-        component={Register}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Drawer.Screen
-        name="Elements"
-        component={ElementsStack}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Drawer.Screen
-        name="Articles"
-        component={ArticlesStack}
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Drawer.Navigator>
-  );
+    return (
+        <Drawer.Navigator
+            style={{flex: 1}}
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            drawerStyle={{
+                backgroundColor: "white",
+                width: width * 0.8,
+            }}
+            drawerContentOptions={{
+                activeTintcolor: "white",
+                inactiveTintColor: "#000",
+                activeBackgroundColor: "transparent",
+                itemStyle: {
+                    width: width * 0.75,
+                    backgroundColor: "transparent",
+                    paddingVertical: 16,
+                    paddingHorizontal: 12,
+                    justifyContent: "center",
+                    alignContent: "center",
+                    alignItems: "center",
+                    overflow: "hidden",
+                },
+                labelStyle: {
+                    fontSize: 18,
+                    marginLeft: 12,
+                    fontWeight: "normal",
+                },
+            }}
+            initialRouteName="Home"
+        >
+            <Drawer.Screen
+                name="Home"
+                component={HomeStack}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Drawer.Screen
+                name="Profile"
+                component={ProfileStack}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Drawer.Screen
+                name="Account"
+                component={Register}
+                options={({ navigation, scene }) => ({
+                    header: () => (
+                        <Header
+                            title="Account"
+                            navigation={navigation}
+                            scene={scene}
+                        />
+                    )
+                })}
+            />
+            <Drawer.Screen
+                name="Elements"
+                component={ElementsStack}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Drawer.Screen
+                name="Articles"
+                component={ArticlesStack}
+                options={{
+                    headerShown: false,
+                }}
+            />
+           <Drawer.Screen
+                name="Text to Sign"
+                component={SignTranslator}
+                options={({ navigation, scene }) => ({
+
+                    header: () => (
+                        <Header
+                            title="🖖Text to Sign"
+                            navigation={navigation}
+                            scene={scene}
+
+                        />
+                    ),
+                })}
+            />
+
+        </Drawer.Navigator>
+
+    );
 }
