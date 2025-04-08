bf83c903e735a9044cad647ea7b2d86cfe7f0695
Sandwhich253989 <avanesh165@gmail.com>
2025-04-08 16:25:39 +0800

    added text to sign

--- navigation/Menu.js
+++ navigation/Menu.js
@@ -13,7 +13,7 @@ function CustomDrawerContent({
   state,
   ...rest
 }) {
-  const screens = ["SignBridgeMain", "Profile", "Account", "LiveTrans", "Articles"];
+  const screens = ["SignBridgeMain", "Profile", "Account", "LiveTrans", "Text To Sign"];
   return (
     <Block
       style={styles.container}
