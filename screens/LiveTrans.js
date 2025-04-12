496cd890f50a6bb907388ed0ba04c13d9a0b09f2
Sandwhich253989 <avanesh165@gmail.com>
2025-04-12 21:08:57 +0800

    updated api for live trans

--- screens/LiveTrans.js
+++ screens/LiveTrans.js
@@ -15,7 +15,7 @@ const LiveTrans = () => {
   const navigation = useNavigation();
   const { height, width } = useWindowDimensions();
 
-  const VM_IP = "http://20.2.65.230:5000";
+  const VM_IP = "https://signtotext.eastasia.cloudapp.azure.com/flaskapp";
 
   const sendFrameToBackend = async () => {
     if (!webcamRef.current) return;
